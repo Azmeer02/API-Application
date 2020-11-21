0331f09079c2d8704abbdc6e6c00481ed14360b5
Syed Azmeer Haider <syedazmeerhaider@gmail.com>
2020-11-22 01:27:30 +0500

    Initial commit

---
+++ src/Covid Components/(2) Stats.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Cards(props){
+    console.log(props)
+    return(
+        <div>
+            {/* {props.data} */}
+        </div>
+    )
+}
+
+export default Cards;
